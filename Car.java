191a5495327f70562b542635c052d0a0ac7c1b35
crazydonut353 <117462853+crazydonut353@users.noreply.github.com>
2025-09-05 09:52:09 -0500

    Update Car.java
    i wish i knew

--- Car.java
+++ Car.java
@@ -1,11 +1,29 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
 public class Car {
     int startMiles; // Starting odometer reading
     int endMiles; // Ending odometer reading
     double gallonsUsed; // Gallons of gas used between the readings
+    
+    public static void main(String[]args){
+        Car auto = new Car(15);
+        System.out.println("New car odometer reading: ");
+        auto.fillUp(150,8);
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+        auto.resetMPG();
+        auto.fillUp(350, 10);
+        auto.fillUp(450, 20);
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+        auto.resetMPG();
+        auto.fillUp(603, 25.5);
+        System.out.println("Miles per gallon: " + auto.calculateMPG());
+    }
 
     // Creates a new instance of a Car object with the starting
     // odometer readings.
-    Car(int startMiles) {
+    Car(int odometerReading) {
         startMiles = odometerReading;
     }
 
@@ -23,8 +41,8 @@ public class Car {
 
     // Resets the myStartMiles to the current odometer reading and
     // resets the gallons used to zero.
-    void resetMPG(int odometerReading) {
-        startMiles = odometerReading;
+    void resetMPG() {
+        startMiles = endMiles;
         gallonsUsed = 0;
     }
 }
